c1f27a5c9a7aeb37e906431aef6f5a41b237401d
Simone João <simonefjoao@gmail.com>
2023-01-20 11:11:16 +0100

    Portifolio Simone João

---
+++ _nuxt/61607e3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{303:function(t,n,e){"use strict";e.r(n);var c={components:{Button:e(132).default},data:function(){return{}}},l=e(9),component=Object(l.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"w-full md:w-1/2"})}),[],!1,null,"20fa3856",null);n.default=component.exports}}]);
